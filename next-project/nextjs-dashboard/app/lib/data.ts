725e637c7532b813d93b9c14f1e60d4eac54a069
DiogoCalmon <diogocalmons@gmail.com>
2025-06-20 08:05:56 -0300

    some changes

--- next-project/nextjs-dashboard/app/lib/data.ts
+++ next-project/nextjs-dashboard/app/lib/data.ts
@@ -16,12 +16,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    // console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -39,9 +39,9 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-      console.log('Fetching revenue data...');
-      await new Promise((resolve) => setTimeout(resolve, 6000));
-      console.log('Data fetch completed after 3 seconds.');
+      // console.log('Fetching revenue data...');
+      // await new Promise((resolve) => setTimeout(resolve, 6000));
+      // console.log('Data fetch completed after 3 seconds.');
 
       // for (let i = 0; i < data.length; i++){
       //   console.log("🥺 ", JSON.stringify(data[i], null, 2));
@@ -76,9 +76,9 @@ export async function fetchCardData() {
       invoiceStatusPromise,
     ]);
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    console.log('Data fetch completed after 3 seconds.');
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 2000));
+    // console.log('Data fetch completed after 3 seconds.');
 
     const numberOfCustomers : number = Number(data[1][0].count ?? '0');
     const numberOfInvoices : number = Number(data[0][0].count ?? '0');    
