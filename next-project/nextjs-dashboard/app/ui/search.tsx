3ab079c6bf344b4307a561f24aa33df2e4247b4e
DiogoCalmon <diogocalmons@gmail.com>
2025-06-24 09:01:02 -0300

    adding debouncing

--- next-project/nextjs-dashboard/app/ui/search.tsx
+++ next-project/nextjs-dashboard/app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const [ searchParams, pathname] = [
@@ -10,8 +11,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
   ]
   const { replace } = useRouter();
 
-  const handleSearch = (term: string) => {
+  const handleSearch = useDebouncedCallback((term: string) => {
+    
     const params = new URLSearchParams(searchParams)
+    params.set('page', '1');
     
     if (term){
       params.set("query", term)
@@ -20,7 +23,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     console.log(params.toString())
     replace(`${pathname}?${params.toString()}`)
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
